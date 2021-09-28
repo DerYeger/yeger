ac84f513f3ab3171f8632bedf252dc02d336b649
Jan Müller <janmueller3698@gmail.com>
2021-09-28 15:24:42 +0200

    build(vite): use `vite-plugin-dts` for declarations

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,17 @@
 import { App, Plugin } from 'vue'
-
-// Import vue component
 import component from '@/masonry-wall.vue'
 
-// Define typescript interfaces for installable component
 type InstallableComponent = typeof component & Plugin
 
-// Default export is installable instance of component.
-// IIFE injects install function into component, allowing component
-// to be registered via Vue.use() as well as Vue.component(),
 const MasonryWall: InstallableComponent =
   /*#__PURE__*/ ((): InstallableComponent => {
-    // Assign InstallableComponent type
-    const installable: InstallableComponent =
-      component as unknown as InstallableComponent
+    const installable = component as unknown as InstallableComponent
 
-    // Attach install function executed by Vue.use()
     installable.install = (app: App) => {
       app.component('MasonryWall', installable)
     }
-    return installable as InstallableComponent
+
+    return installable
   })()
 
 export default MasonryWall
-
-// It's possible to expose named exports when writing components that can
-// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
-// export const RollupDemoDirective = directive;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
-import typescript from 'rollup-plugin-typescript2'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    typescript({
-      useTsconfigDeclarationDir: true,
+    dts({
+      cleanVueFileName: true,
+      include: 'src/**',
+      outputDir: 'dist/types',
+      staticImport: true,
     }),
   ],
   resolve: {
