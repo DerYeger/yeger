eab6139d88fcf9b44bd5baec13609ef226270447
Jan Müller <janmueller3698@gmail.com>
2021-09-28 10:19:55 +0200

    build: use `vite` for bundling

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,25 @@ import { App, Plugin } from 'vue'
 import component from '@/masonry-wall.vue'
 
 // Define typescript interfaces for installable component
-type InstallableComponent = typeof component & {
-  install: Exclude<Plugin['install'], undefined>
-}
+type InstallableComponent = typeof component & Plugin
 
 // Default export is installable instance of component.
 // IIFE injects install function into component, allowing component
 // to be registered via Vue.use() as well as Vue.component(),
-export default /*#__PURE__*/ ((): InstallableComponent => {
-  // Assign InstallableComponent type
-  const installable = component as unknown as InstallableComponent
+const MasonryWall: InstallableComponent =
+  /*#__PURE__*/ ((): InstallableComponent => {
+    // Assign InstallableComponent type
+    const installable: InstallableComponent =
+      component as unknown as InstallableComponent
 
-  // Attach install function executed by Vue.use()
-  installable.install = (app: App) => {
-    app.component('MasonryWall', installable)
-  }
-  return installable
-})()
+    // Attach install function executed by Vue.use()
+    installable.install = (app: App) => {
+      app.component('MasonryWall', installable)
+    }
+    return installable as InstallableComponent
+  })()
+
+export default MasonryWall
 
 // It's possible to expose named exports when writing components that can
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
