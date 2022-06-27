d5c2a095b25c3a4f19caa5acc3e5bfe37e6ebbb4
Valery Sinevich <zohan33333@gmail.com>
2022-06-28 01:33:34 +0300

    change fb unit to px

--- src/masonry-wall.vue
+++ src/masonry-wall.vue
@@ -11,7 +11,7 @@
       :data-index="columnIndex"
       :style="{
         display: 'flex',
-        'flex-basis': '0%',
+        'flex-basis': '0px',
         'flex-direction': 'column',
         'flex-grow': 1,
         height: ['-webkit-max-content', '-moz-max-content', 'max-content'],
