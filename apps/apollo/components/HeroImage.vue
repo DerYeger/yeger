28d5365b62ba7ce070380ed9af53ea97857f1a6c
Jan Müller <janmueller3698@gmail.com>
2022-12-31 09:48:04 +0100

    feat(apollo): improve hero image position

--- apps/apollo/components/HeroImage.vue
+++ apps/apollo/components/HeroImage.vue
@@ -5,18 +5,18 @@ const { accessibleDescription } = useLogo()
 <template>
   <figure
     v-once
-    class="relative w-72 sm:w-96 md:w-128 xl:w-172 pb-100% max-w-full children:select-none"
+    class="relative mt-12.5% mb--12.5% md:ml-12.5% w-72 sm:w-96 md:w-128 xl:w-172 pb-100% max-w-full children:select-none"
   >
     <img
       src="/hero-first.webp"
       :alt="accessibleDescription"
-      class="drop-shadow-custom absolute"
+      class="drop-shadow-custom absolute translate-x--12.5% translate-y--12.5%"
       sizes="sm:512px md:512px xl:688px"
     />
     <img
       src="/hero-second.webp"
       :alt="accessibleDescription"
-      class="drop-shadow-custom absolute translate-x-25% translate-y-27.5%"
+      class="drop-shadow-custom absolute translate-x-12.5% translate-y-12.5%"
       sizes="sm:512px md:512px xl:688px"
     />
     <figcaption class="display-none">{{ accessibleDescription }}</figcaption>
