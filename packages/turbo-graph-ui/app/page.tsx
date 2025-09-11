1bb1e2fa8423416428de0a57999a1c61ad04f50c
Jan Müller <janmueller3698@gmail.com>
2025-09-11 21:07:51 +0200

    fix(turbo-graph-ui): fetch tasks on render

--- packages/turbo-graph-ui/app/page.tsx
+++ packages/turbo-graph-ui/app/page.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
   icons: ['/favicon.ico'],
 }
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const tasks = await getAllTasks()
 
